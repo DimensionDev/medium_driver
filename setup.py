202e40779add99369df52a4b5ebbb7fcdb6cccde
Mindey <mindey@qq.com>
2019-01-06 13:32:15 +0000

    update generator

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='mediumdb',
-    version='0.0.2',
+    version='0.0.3',
     description='Medium driver.',
     url='https://github.com/sujitech/medium_driver',
     author='Mindey',
